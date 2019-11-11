4dfa729298fe55da6c5c85887b3f02a74167b061
caleblareau <caleblareau@g.harvard.edu>
2019-11-10 22:14:18 -0500

    setup

---
+++ data/README.md
@@ -0,0 +1,4 @@
+Data from:
+https://support.10xgenomics.com/single-cell-atac/datasets/1.1.0/atac_pbmc_1k_nextgem
+
+
---
+++ data/wget_bigger_data.sh
@@ -0,0 +1,1 @@
+wget http://cf.10xgenomics.com/samples/cell-atac/1.1.0/atac_pbmc_1k_nextgem/atac_pbmc_1k_nextgem_fragments.tsv.gz
